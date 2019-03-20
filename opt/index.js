af31fba63cfca8807949eb51a2f02402ee120e72
jdtzmn <jdtzmn@gmail.com>
2019-03-20 19:06:07 -0400

    Some small optimizations

--- opt/index.js
+++ opt/index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/env node
 
 var fs = require('fs')
 var http = require('http')
@@ -8,7 +8,9 @@ var log = fs.readFileSync('ngrok.log', {encoding: 'utf-8'})
 var regex = /URL:tcp:\/\/0.tcp.ngrok.io:(\d{5})/g
 var port = regex.exec(log)[1]
 
-http.createServer(function (request, response) {
+const listener = http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' })
   response.end('Server Address: 0.tcp.ngrok.io:' + port)
-}).listen(process.env.PORT || 8080)
+}).listen(process.env.PORT || 8080, () => {
+  console.log(`Web server is listening on port: ${listener.address().port}`)
+})
